f0ff20e83e5000143d277f7b27e86268a18a27ed
userwin <worfysito15@gmail.com>
2026-01-13 15:42:07 -0500

    coneccion db

--- model/conexion.php
+++ model/conexion.php
@@ -4,9 +4,9 @@ class Conexion
   static public function conn()
   {
     /* base de datos Docker - usar nombre del servicio 'db' */
-    $link = new PDO("mysql:host=db;dbname=pruebasbetaacide_marsa", "root", "test");
+    /* $link = new PDO("mysql:host=db;dbname=pruebasbetaacide_marsa", "root", "test"); */
     /* base de datos cpanel */
-    /* $link = new PDO("mysql:host=localhost;dbname=pruebasbetaacide_marsa", "pruebasbetaacide_admin_marsa", "&4ptnm!9dqdHM5m4"); */
+    $link = new PDO("mysql:host=localhost;dbname=pruebasbetaacide_marsa", "pruebasbetaacide_admin_marsa", "&4ptnm!9dqdHM5m4");
     $link->exec("set names utf8");
     return $link;
   }
